938a3f1a5b5cf9019219b9073cde8c4138a2bb2d
jkj8982@gmail.com <jkj>
2020-02-29 13:18:38 +0900

    리팩토링

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,11 +1,27 @@
 public class Game {
-    private int score = 0;
+    private int[] rolls = new int [21];
+    private int currentRoll = 0;
 
     public void roll(int pins) {
-        this.score += pins;
+        rolls[currentRoll++] = pins;
     }
 
     public int getScore() {
-        return this.score;
+        int score = 0;
+        int firstRollInFrame = 0;
+        for(int frame = 0; frame < 10; frame++) {
+            if(isSpare(firstRollInFrame)) {
+                score += 10 + rolls[firstRollInFrame + 2];
+                firstRollInFrame += 2;
+            } else {
+                score += rolls[firstRollInFrame] + rolls[firstRollInFrame + 1];
+                firstRollInFrame += 2;
+            }
+        }
+        return score;
+    }
+
+    private boolean isSpare(int firstRollInFrame) {
+        return rolls[firstRollInFrame] + rolls[firstRollInFrame + 1] == 10;
     }
 }
